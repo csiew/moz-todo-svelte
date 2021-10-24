93f03b04fca82773db10180fc2ab6a89ec39c34a
Clarence Siew <clarence.siew@gmail.com>
2021-10-24 22:51:37 +1100

    Wrap store with local storage helper

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,6 @@
 	import Todos from './components/Todos.svelte';
 
 	import { todos } from './stores';
-
-	$todos = [
-    {
-      id: 1,
-      name: 'Create a Svelte starter app',
-      completed: true
-    },
-    {
-      id: 2,
-      name: 'Create your first component',
-      completed: true
-    },
-    {
-      id: 3,
-      name: 'Complete the rest of the tutorial',
-      completed: false
-    }
-  ];
 </script>
 
 <main>
--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,11 @@
 import { writable } from 'svelte/store';
+import { localStore } from './localStore';
 
 export const alert = writable('Welcome to To-Do!');
-export const todos = writable([]);
+
+const initialTodos = [
+  { id: 1, name: 'Visit MDN web docs', completed: true },
+  { id: 2, name: 'Complete the Svelte Tutorial', completed: false },
+];
+
+export const todos = localStore('mdn-svelte-todo', initialTodos);
